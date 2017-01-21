1fc335ddbe4f16c0f7388a515a7b06b0b2e13994
XuJun <nankaimathxujun@gmail.com>
2017-01-21 13:08:08 +0800

    add

--- batchprocessing/Image2Patch.m
+++ batchprocessing/Image2Patch.m
@@ -1,17 +1,19 @@
-function       [X, Sigma] = Image2Patch( im_out, im_in, par)
+function       [X, Sigma] = Image2Patch( im_out, im_in, par )
 % record the non-local patch set and the index of each patch in
 % of seed patches in image
 im_out         =  single(im_out);
-X          =  zeros(par.ps^2, par.maxr*par.maxc, 'single');
-NX          =  zeros(par.ps^2, par.maxr*par.maxc, 'single');
+X          =  zeros(par.ps2, par.maxrc, 'single');
+NX          =  zeros(par.ps2, par.maxrc, 'single');
 k    =  0;
-for i  = 1:par.ps
-    for j  = 1:par.ps
-        k    =  k+1;
-        blk  = im_out(i:end-par.ps+i,j:end-par.ps+j);
-        nblk  = im_in(i:end-par.ps+i,j:end-par.ps+j);
-        X(k,:) = blk(:)';
-        NX(k,:) = nblk(:)';
+for l = 1:par.ch
+    for i = 1:par.ps
+        for j = 1:par.ps
+            k    =  k+1;
+            blk  = im_out(i:end-par.ps+i,j:end-par.ps+j, l);
+            nblk  = im_in(i:end-par.ps+i,j:end-par.ps+j, l);
+            X(k,:) = blk(:)';
+            NX(k,:) = nblk(:)';
+        end
     end
 end
 Sigma = sqrt(abs(repmat(par.nSig0^2, 1, size(X,2)) - mean((NX - X).^2))); %Estimated Local Noise Level
--- batchprocessing/PGs2Image.m
+++ batchprocessing/PGs2Image.m
@@ -1,4 +1,4 @@
-function      im_out = PGs2Image(X_hat, W, par)
+function      im_out = PGs2Image(X, W, par)
 % Reconstruction
 im_out = zeros(par.h, par.w, par.ch);
 im_wei = zeros(par.h, par.w, par.ch);
@@ -9,7 +9,7 @@ for l = 1:1:par.ch
     for i = 1:1:par.ps
         for j = 1:1:par.ps
             k = k+1;
-            im_out(r-1+i, c-1+j, l)  =  im_out(r-1+i, c-1+j, l) + reshape( X_hat(k,:)', [par.maxr par.maxc] );
+            im_out(r-1+i, c-1+j, l)  =  im_out(r-1+i, c-1+j, l) + reshape( X(k,:)', [par.maxr par.maxc] );
             im_wei(r-1+i, c-1+j, l)  =  im_wei(r-1+i, c-1+j, l) + reshape( W(k,:)', [par.maxr par.maxc] );
         end
     end
