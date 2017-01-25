41a17cfb8abf553efe9558e153f093383f8331de
XuJun <nankaimathxujun@gmail.com>
2017-01-25 09:06:49 +0800

    add

--- batchprocessing/WLSWSC.m
+++ batchprocessing/WLSWSC.m
@@ -1,21 +1,34 @@
-% weighted least square and weighted sparse coding
-function  X = WLSWSC(Y, Wei, C, par)
+% double weighted: weighted least square and weighted sparse coding framework
+function  X = WLSWSC(Y, Wei, par)
 X = Y;
-% update Wei
-% IS NOT NEED
+% initialize D and S
+[D, S, ~] = svd(full(Y), 'econ');
+S = diag(S);
+f = 0;
 for i=1:par.WWIter
+        f_prev = f;
+    % update W for weighted sparse coding
+    Wsc = bsxfun(@rdivide, par.lambda * Wls.^ 2, sqrt(S) + eps );
+    % update C by soft thresholding
+    B = D' * Y;
+    C = sign(B) .* max(abs(B) - Wsc, 0);
     % update D and S
     CW = bsxfun(@times, C, Wei);
     YW = bsxfun(@times, X, Wei);
     [U, S, V] = svd( CW * YW', 'econ');
     D = U * V';
     S = diag(S);
-    % update
-    Wsc = bsxfun(@rdivide, par.lambda * (Wei .^ 2), sqrt(S) + eps );
-    % update C
-    B = D' * Y;
-    C = sign(B) .* max(abs(B) - Wsc, 0);
-    % update X
-    X = D * C;
+    
+    DT = (Y - D * C) * Wei;
+    DT = DT(:)'*DT(:) / 2;
+    RT = Wsc .*  C;    
+    RT = norm(RT, 1);
+    f_curr = DT + RT;
+    if (abs(f_prev - f_curr) / f_curr < par.epsilon) ||
+        break;
+    end
+    fprintf('WLSWSC Energy: %d\n',f);
 end
+% update X
+X = D * C;
 return;
