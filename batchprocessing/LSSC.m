937f242e7ceb9b1eee13074c64b26c970e0e100d
XuJun <nankaimathxujun@gmail.com>
2017-01-25 21:19:20 +0800

    add

--- batchprocessing/LSSC.m
+++ batchprocessing/LSSC.m
@@ -1,36 +1,22 @@
 % double weighted: weighted least square and weighted sparse coding framework
 function  X = LSSC(Y, par)
-% initialize D and S
-[U, S, V] = svd(Y * Y', 'econ');
-D = U * V';
-% S = diag(S);
+% initialize D as identity matrix
+D = eye(size(Y, 1));
 f_curr = 0;
 for i=1:par.WWIter
     f_prev = f_curr;
-    % update W for weighted sparse coding
-    %     Wsc = bsxfun(@rdivide, par.lambdasc * Wls .^ 2, sqrt(S) + eps );
     % update C by soft thresholding
     B = D' * Y;
-    %     C = sign(B) .* max(abs(B) - Wsc, 0);
     C = sign(B) .* max(abs(B) - par.lambdasc, 0);
-    % update D and S
+    % update D
     [U, ~, V] = svd( C * Y', 'econ');
-    D = U * V';
-    %     S = diag(S);
-    
-    %     residual = norm(C - C_prev, 1);
-    %     fprintf('C residual, %d th: %2.8f\n', i, residual);
-    %     if residual < par.epsilon
-    %         %     if (abs(f_prev - f_curr) / f_curr < par.epsilon)
-    %         break;
-    %     end
-    %     DT = bsxfun(@times, Y - D * C, Wls);
+    D = V * U';
+    % energy function
     DT = Y - D * C;
     DT = norm(DT, 'fro');
-    %     RT = Wsc .*  C;
     RT = sum(sum(abs(C)));
     f_curr = 0.5 * DT^2 + par.lambdasc * RT;
-    fprintf('WLSWSC Energy, %d th: %2.8f\n', i, f_curr);
+    fprintf('LSSC Energy, %d th: %2.8f\n', i, f_curr);
     if (abs(f_prev - f_curr) / f_curr < par.epsilon)
         break;
     end
