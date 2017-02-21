ede2d272b4a10062c4faa1b60508e545fed28aeb
XuJun <nankaimathxujun@gmail.com>
2017-02-21 18:58:13 +0800

    add

---
+++ batchprocessing/WLSWSC_NL_1AR.m
@@ -0,0 +1,55 @@
+function  [im_out, par] = WLSWSC_NL_1AR(par)
+im_out    =   par.nim;
+par.nSig0 = par.nSig;
+% parameters for noisy image
+[h,  w, ch]      =  size(im_out);
+par.h = h;
+par.w = w;
+par.ch = ch;
+par = SearchNeighborIndex( par );
+% original noisy image to patches
+NY = Image2PatchNew( par.nim, par );
+for ite  =  1 : par.outerIter
+    %     % iterative regularization
+    %     im_out = im_out+par.delta*(par.nim - im_out);
+    % image to patches and estimate local noise variance
+    Y = Image2PatchNew( im_out, par );
+    % estimate local noise variance, par.lambdals is put here since the MAP
+    % and Bayesian rules
+    Sigma = sqrt(abs(repmat(par.nSig0^2, 1, size(Y, 2)) - mean((NY - Y).^2))); 
+    % estimation of noise variance
+    if mod(ite-1, par.innerIter)==0
+        par.nlsp = par.nlsp - 10;
+        % searching  non-local patches
+        blk_arr = Block_Matching( Y, par );
+        if ite == 1
+            Wls = par.nSig0 * ones(size(Sigma));
+        end
+    end
+    % Weighted Sparse Coding
+    Y_hat = zeros(par.ps2ch, par.maxrc, 'single');
+    W_hat = zeros(par.ps2ch, par.maxrc, 'single');
+    for i = 1:par.lenrc
+        index = blk_arr(:, i);
+        nlY = Y( : , index );
+        DC = mean(nlY, 2);
+        nDCnlY = bsxfun(@minus, nlY, DC);
+        nDCnlYhat = WLSWSC(nDCnlY, Wls(index), par);
+        % update weight for least square
+        Wls(index) = exp( - par.lambdals1 * sqrt(sum((nDCnlY - nDCnlYhat) .^2, 1)) );
+        nlYhat = bsxfun(@plus, nDCnlYhat, DC);
+        % add DC components and aggregation
+        Y_hat(:, index) = Y_hat(:, index) + nlYhat;
+        W_hat(:, index) = W_hat(:, index) + ones(par.ps2ch, par.nlsp);
+    end
+    % Reconstruction
+    im_out = PGs2Image(Y_hat, W_hat, par);
+    % calculate the PSNR
+    PSNR =   csnr( im_out * 255, par.I * 255, 0, 0 );
+    SSIM      =  cal_ssim( im_out * 255, par.I * 255, 0, 0 );
+    fprintf('Iter %d : PSNR = %2.4f, SSIM = %2.4f\n', ite, PSNR, SSIM);
+    par.PSNR(ite, par.image) = PSNR;
+    par.SSIM(ite, par.image) = SSIM;
+end
+return;
+
