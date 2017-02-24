34dce60c3c596516c4b763dcf2de80fffddcebb2
XuJun <nankaimathxujun@gmail.com>
2017-02-24 19:29:51 +0800

    add

--- batchprocessing/WLSSC_DCW.m
+++ batchprocessing/WLSSC_DCW.m
@@ -1,14 +1,15 @@
 % single weighted: weighted least square and sparse coding framework
-function  X = WLSSC(Y, Wls, par)
+function  X = WLSSC_DCW(Y, Wls, par)
 % initialize D
 YW = bsxfun(@times, Y, Wls);
 [U, ~, V] = svd(YW * YW', 'econ');
 D = V * U';
-% update W for weighted sparse coding
-Wsc = par.lambdasc *par.Sigma ./Wls.^2;
 f_curr = 0;
+Wls = 
 for i=1:par.WWIter
     f_prev = f_curr;
+    % update W for weighted sparse coding
+    Wsc = par.lambdasc * Wls.^2;
     % update C by soft thresholding
     B = D' * Y;
     C = sign(B) .* max(abs(B) - repmat(Wsc, [size(B, 1), 1]), 0);
@@ -24,13 +25,15 @@ for i=1:par.WWIter
         [U, ~, V] = svd( CW * YW', 'econ');
     end
     D = V * U';
+    % update weight for least square
+    Wls = par.lambdals ./ sqrt(sum((nDCnlY - D * C) .^2, 1));
     % energy function
     DT = bsxfun(@times, Y - D * C, Wls);
     DT = norm(DT, 'fro');
     %     DT = DT(:)'*DT(:);
     RT = sum(sum(abs(C)));
     f_curr = 0.5 * DT ^ 2 + par.lambdasc * RT;
-%     fprintf('WLSSC Energy, %d th: %2.8f\n', i, f_curr);
+    fprintf('WLSSC Energy, %d th: %2.8f\n', i, f_curr);
     if (abs(f_prev - f_curr) / f_curr < par.epsilon)
         break;
     end
